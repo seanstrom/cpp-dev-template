64014c58f5dfce06e214997308a072feeb73578d
Sean Hagstrom <sean@seanhagstrom.com>
2022-07-17 11:04:59 +0100

    use clang for C++20 with modules

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,7 @@
 add_rules("mode.debug", "mode.release")
+set_languages("c++20")
 
 target("hello")
     set_kind("binary")
-    set_toolchains("gcc-11")
+    set_toolchains("clang")
     add_files("src/*.cpp", "src/*.mpp")
